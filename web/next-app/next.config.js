d22633b209c362c598f011d24f24f0eeb077ff18
chuma-beep <wisboynelson123@gmail.com>
2026-05-13 19:52:41 +0100

    made a config rewrite

--- web/next-app/next.config.js
+++ web/next-app/next.config.js
@@ -2,6 +2,11 @@
 
 const nextConfig = {
   output: 'export',
+  async rewrites() {
+    return [
+      { source: '/api/:path*', destination: 'http://localhost:8080/api/:path*' },
+    ]
+  },
 }
 
 module.exports = nextConfig
